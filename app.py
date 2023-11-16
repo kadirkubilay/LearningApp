68970f2922f18232a80fb7fd4fb619ded9eb4012
glzolk <glynis@zolk.eu>
2023-11-16 13:30:32 +0100

    Secure OpenAI Key

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = 'KADOKUBIERDEM'
 
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ["OPENAI_API"]
 
 # Article content
 article = """
